8ca86f7a9698ad4f60a072cc574f288a4911c78b
pingchen09990102 <pingchen09990102@users.noreply.github.com>
2016-07-28 11:31:53 +0800

    Update Fragile.pm

--- BMSIM1.0/lib/Fragile.pm
+++ BMSIM1.0/lib/Fragile.pm
@@ -4,74 +4,63 @@ package Fragile;
 use strict;
 use warnings;
 use Math::Random qw(:all);
-#use constant PI => 3.1415926536;
-#use constant SIGNIFICANT => 5; # number of significant digits to be returned
 
 sub FragileSite 
+{
+	my ($a,$b,$PosSet,$DistanceSet) = @_; 
+	my $PosSet_length=@$PosSet;#position of fragile site
+	my @FragilePos=(); 
+	my ($p,$rbinom,$x)=0;
+	for (my $l=0;$l<$PosSet_length;$l++) 
 	{
+		#if ($$OriSet[$l] eq "+") #type I fragile
+		#{
+			#completely break
+			#$p=0;
 
-		#my ($mean1,$sd1,$mean2,$sd2,$PosSet,$DistanceSet,$OriSet) = @_; 
-		my ($a,$b,$PosSet,$DistanceSet) = @_; 
+			#don't break
+			#$p=1;
 
-		my $PosSet_length=@$PosSet;
-		my @FragilePos=(); 
-		my ($p,$rbinom,$x)=0;
-		for (my $l=0;$l<$PosSet_length;$l++) 
-		{
-			#先运行易碎点计算perl
-			#判断易碎点类型
-			#if ($$OriSet[$l] eq "+") #type I fragile
-			#{
-				#完全断裂
-				#$p=0;
+			#cumulative normal model
+			#$Nlabel_size=($$DistanceSet[$l]-$mean1)/$sd1;
+			#$p=1-&uprob ($Nlabel_size);
 
-				#完全不断裂
-				#$p=1;
+			#weibull model 
+			#$x=$Nlabel_size/100;
+			#$p=$a*$b*$x**($b-1)*exp(-$a*$x**$b)
+			#my $p=$a*$b*$x**($b-1)*exp(-$a*$x**$b);
 
-				#累积正态断裂法
-				#$Nlabel_size=($$DistanceSet[$l]-$mean1)/$sd1;#先将易碎点间距标准化
-				#$p=1-&uprob ($Nlabel_size);#根据距离计算易碎点不断的概率，长度越长不断即成功概率越大
+			#Exponential model
+			$x=$$DistanceSet[$l];
+			$p=1-$a*exp($x*$b);
+			print "x=$x;p=$p\n";
 
-				#韦伯分布断裂法
-				#$x=$Nlabel_size/100;#将长度除以100使其小于10
-				#$p=$a*$b*$x**($b-1)*exp(-$a*$x**$b)#根据weibull公式计算断裂概率，a,b分别为位置参数和尺度参数
-				#my $p=$a*$b*$x**($b-1)*exp(-$a*$x**$b);#根据weibull公式计算断裂概率，a,b分别为位置参数和尺度参数
-
-				#指数分布断裂法得到成功概率
-				$x=$$DistanceSet[$l];
-				$p=1-$a*exp($x*$b);#根据指数分布公式计算断裂概率，1-断裂概率得成功概率用于白努力实验
-				print "x=$x;p=$p\n";
-
-				#进行白努力实验
-				#$rbinom=1;
-				$rbinom=random_binomial(1,1,$p);#根据前面算的概率进行白努力实验，
-			#}
-			#else #type II fragile
-			#{
-				#累积正态断裂法
-				#$Nlabel_size=($$DistanceSet[$l]-$mean2)/$sd2;#先将易碎点间距标准化
-				#$p=1-&uprob ($Nlabel_size);#根据距离计算易碎点不断的概率，长度越长不断即成功概率越大
+			#$rbinom=1;
+			$rbinom=random_binomial(1,1,$p);
+		#}
+		#else #type II fragile
+		#{
+			#cumulative normal model
+			#$Nlabel_size=($$DistanceSet[$l]-$mean2)/$sd2;
+			#$p=1-&uprob ($Nlabel_size);
 				
-				#完全断裂
-				#$p=0;
+			#completely Break
+			#$p=0;
 
-				#$rbinom=1;
-				#$rbinom=random_binomial(1,1,$p);#根据前面算的概率进行白努力实验，
-			#}
-			if ($rbinom==1)#为1的时候表示不断，此处不加X
-			{
-				next;
-			}
-			else #不为1的时候表示断了，调用替换脚本对应位点处碱基序列变为X
-			{
-				push (@FragilePos,$$PosSet[$l]);#把断裂位置存入数组
-			}
-		
+			#$rbinom=1;
+			#$rbinom=random_binomial(1,1,$p);，
+		#}
+		if ($rbinom==1)#don't break when it is 1
+		{
+			next;
 		}
-		return (@FragilePos);#返回断裂位置数组
+		else #break when it is not 1, sign the corresponding position with X
+		{
+			push (@FragilePos,$$PosSet[$l]);
+		}
+		
+	}
+	return (@FragilePos);
+}
 
-	}#对应sub
-
-
-
-1
+1;
