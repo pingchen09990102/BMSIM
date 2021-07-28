39cc8f7dae3b70a45c61288f0949d9e2960c45bb
pingchen09990102 <pingchen09990102@users.noreply.github.com>
2021-07-28 19:36:08 +0800

    Update BMSIM.pl

--- BMSIM1.0/BMSIM.pl
+++ BMSIM1.0/BMSIM.pl
@@ -16,14 +16,6 @@ use Digest;
 use RES;
 use Fragile;
 
-#########################################################################################################################################################################################################################
-#example：                                
-#
-#	
-# perl BMSIM2-5.pl -cov 10 -p PLsimCov350 -ca MG1655.fa -bnx PLsimCov350.bnx -fragile MG1655.txt -e GCTCTTC,GAAGAGC -np1 7 >log.txt
-#
-#
-##########################################################################################################################################################################################################################
 
 
 my $start_time=time();
@@ -629,66 +621,82 @@ sub PRINT
 ################################################################################
 ##############                  Documentation                 ##################
 ################################################################################
-## style adapted from http://www.perlmonks.org/?node_id=489861 
+##  
 __END__
 
 =head1 NAME
 
+BMSIM is a simulation tool for BioNano molecule data of the BioNano optical mapping platform
 
 =head1 USAGE
 
 perl BMSIM.pl [options]
 
-#example：
+Example 1：
+#single nicking: NtBspQI: 5' ...GCTCTTCN^... 3' the nick position is 7;    
+#                         3' ...CGAGAAGN... 5'                            
+perl BMSIM.pl -cov 10 -ca ./example/MG1655.fa -fragile ./example/MG1655.txt -e GCTCTTC,GAAGAGC -np1 7 -p EcoliSim1 -bnx out1.bnx >log.txt
 
-perl BMSIM.pl -cov 10 -p EcoliSim -ca MG1655.fa -bnx EcoliSim.bnx -fragile MG1655.txt -e GCTCTTC,GAAGAGC -np1 7 >log.txt
+Example 2：
+#Dual nicking  NtBspQI: 5' ...GCTCTTCN^... 3' the nick position is 7;    Nb.BsrDI: 5' ...GCAATGNN... 3'  the nick position is -1.
+#                       3' ...CGAGAAGN... 5'                                       3' ...CGTTAC^NN... 5'
+perl BMSIM.pl -cov 10 -ca ./example/MG1655.fa -fragile ./example/MG1655.txt -e GCTCTTC,GAAGAGC,GCAATG,CATTGC  -np1 7 -np2 -1 -p EcoliSim2 -bnx out2.bnx >log.txt
+
+Example 3：
+#Change parameters (mean and standard deviation) for the stretch model:
+perl BMSIM.pl -cov 10 -ca ./example/MG1655.fa -fragile ./example/MG1655.txt -e GCTCTTC,GAAGAGC -np1 7 -str 0.95,0.05 -p EcoliSim3 -bnx out3.bnx >log.txt
 
 
 =head1 OPTIONS
 
-Documentation options: 
+perl BMSIM.pl [options]
 
--help brief help message
+Documentation options:
+
+-help	brief help message
 
 Required options:
 
--cov	Coverage
--ca	reference genome fasta
--bnx	bnx output
--fragile	fragile site file
-
+-cov,--Coverage int	Coverage depth for the simulated genome, e.g. 10, represent simulating 10X coverage depth for the simulated genome
+-ca,--chr_fa string	file with genome sequence(.fa), e.g. example/MG1655.fa
+-fragile,--Fragile_infile string	file with fragile site, e.g. example/MG1655.txt
+-bnx,--bnx_outfile string	output file(.bnx), e.g. example/EcoliSim.bnx
 
 Optional options:
 
--lm	length_mean,#possion
+-lm,--length_mean int	mean length (kb) of BioNano molecule, produce BioNano molecular length according to the exponential distribution (Default:70)
 
--FNp	FN_probality,Binomil
--FPi	FP_intensity,possion~lamda
+-FNp,--FN_probability float	parameter for the false negative site model, i.e. Binomil distibution, FNp is the probability of getting a sucess in one trial (Default:0.85)
 
--str	stretch_normal,stretch~N(ave,std),comma-separated lists of values
+-FPi,--FP_intensity float	the initial interval of false positive site (kb) (Default:100)
 
--Rt	Resolution type
--RtI	Res_typeI, under1000,under1500
--RtII	Res_typeII, resolution~N(ave,std)
+-str,--stretch_normal string	parameters (ave,std) for the stretch model,i.e. Normal distribution, Stretch~N(ave,std), ave is mean, std is standard deviation, comma-separated lists of values (Default:0.98,0.08)
 
--snr	SNR, SNR~N(ave,std)
--Ints	Intensity, Intensity~N(ave,std)
+-Rt,--Res_type int	resolution type, "1" for "Res_typeI", "2" for "Res_typeII" (Default:2)
 
--Chi_pert	Chi_percent
--Chi_Bi		Chi_Bi
--Chi_Tr		Chi_Tri
--Chi_Qua	Chi_Qua
+-RtI,--Res_typeI string	resolution model type I:p1,p2, p1 is the proportion of sites within 1kb distance that are retained, p2 is the proportion of sites within 1.5kb distance that are retained, comma-separated lists of the values p1 and p2 (Default:0,0.9)
+-RtII,--Res_typeII string	parameters (ave,std) for the resolution model type II,i.e. Normal distribution, Res_typeII~N(ave,std), ave is mean, std is standard deviation, comma-separated lists of values (Default:1.2,0.9)
 
--e	enzyme nicking pattern
--np1	nick_position1, nicking position of the enzyme pattern in 5' to 3',
+-snr,--SNR string	parameters (ave,std) for signal noise ratio (SNR) model of BioNano molecule,i.e. Normal distribution, SNR~N(ave,std), ave is mean, std is standard deviation, comma-separated lists of values (Default:3,0.66)
+-Ints,--Intensity string	parameters (ave,std) for Intensity model of BioNano molecule, i.e. Normal distribution, Intensity~N(ave,std), ave is mean, std is standard deviation, comma-separated lists of values (Default:1,0.2)
+
+
+-Chi_prop,--Chimeric_proportion	float	proportion of the total chimeric molecule in raw BioNano molecule (Default:0.1)
+-Chi_Bi,--Bimera_proportion float	proportion of Bimera (Default:0.85)
+-Chi_Tr,--Trimera_proportion	float	proportion of Trimera (Default:0.13)
+-Chi_Qua,--Quadramera_proportion float	proportion of Quadramera (Default:0.02)
+
+
+-e,--enzymes_pattern string	nicking pattern of the enzyme, e.g. GCTCTTC,GAAGAGC for NtBspQI; GCAATG,CATTGC for Nb.BsrDI; GCTCTTC,GAAGAGC,GCAATG,CATTGC for dual nicking with NtBspQI and Nb.BsrDI (Default:GCTCTTC,GAAGAGC)
+-np1,--nick_position1 int	nicking position of the enzyme pattern from 5' to 3' for enzyme 1,
          e.g. NtBspQI: 5' ...GCTCTTCN^... 3' the np1 is 7;    Nb.BsrDI: 5' ...GCAATGNN... 3'  the np1 is -1.
-                       3' ...CAGAGNN... 5'                              3' ...CGTTAC^NN... 5'
-		 
--np2	nick_position2
--f	FragileTypeI
--p	project name for all assemblies
+                       3' ...CGAGAAGN... 5'                             3' ...CGTTAC^NN... 5'	 
+-np2,--nick_position2 int	nicking position of the enzyme pattern from 5' to 3' for enzyme 2,
+         e.g. NtBspQI: 5' ...GCTCTTCN^... 3' the np2 is 7;    Nb.BsrDI: 5' ...GCAATGNN... 3'  the np2 is -1.
+                       3' ...CGAGAAGN... 5'                             3' ...CGTTAC^NN... 5'
+
+-p,--proj string	project name for the simulation, e.g. EcoliSim
 
 
 =cut
-
 
